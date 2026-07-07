733f58bcbf711e07b5e42405e33005c7d692ef09
inscope-labs <afterdawn.service@gmail.com>
2026-07-06 23:58:29 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,11 +10,11 @@ plugins {
 }
 
 android {
-  namespace = "com.example"
+  namespace = "com.inscopelabs.abxmcp"
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.abxmcp.kptqzx"
+    applicationId = "com.inscopelabs.abxmcp.main"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
